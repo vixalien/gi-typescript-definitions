e4d19125c207b9f388e028f1be65357037c6a266
Angelo Verlain <geoangercola@gmail.com>
2024-07-28 23:42:40 +0200

    chore: only provide access to pwd

--- generate_types_in_flatpak.sh
+++ generate_types_in_flatpak.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Entering the GNOME master flatpak runtime..."
-FLATPAK_ENABLE_SDK_EXT=node20 flatpak run --share=network --command=./generate_types.sh --filesystem=host org.gnome.Sdk//master
+FLATPAK_ENABLE_SDK_EXT=node20 flatpak run --share=network --command=./generate_types.sh --filesystem=$(pwd)  org.gnome.Sdk//master
